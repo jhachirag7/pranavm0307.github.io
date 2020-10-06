8ce585250af86a5f708b88bf6815f0ff07cd4710
pranavm0307 <71208813+pranavm0307@users.noreply.github.com>
2020-10-06 12:43:52 +0530

    Create qr code.js

---
+++ qr code.js
@@ -0,0 +1,27 @@
+
+
+const App = () =>{
+    const [name,changedName] = React.useState("Nafi")
+    const result = (event) =>{
+        changedName(event.target.value)
+    }
+    
+  return (
+     
+     <div className="container">
+        <h1>QR CODE GENERATOR</h1>
+        <input type="text"
+        onChange={result}
+        />
+        
+        <img        src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${name}`}
+        alt="QRcode"
+        className="img"/>
+        
+          </div>
+  )
+}
+ReactDOM.render(
+ <App/>,
+  document.getElementById('react-app')
+);
